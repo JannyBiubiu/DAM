e299765f7ed366e0b2a33ad19ebab3314e100beb
JannyBiubiu <3160102525@zju.edu.cn>
2018-11-07 17:15:23 +0800

    finish HW, but watermarking not yet

--- myapp/views.py
+++ myapp/views.py
@@ -4,17 +4,20 @@ from flask import render_template,request,jsonify
 from myapp import app
 import base64
 import cv2
-import werkzeug
 import os
 import json
 import numpy as np
 from PIL import Image
-import io
+
 def bs64_cv(str):
 	offset=str.find('base64,')
 	str = base64.b64decode(str[(offset+7):])
 	nparr = np.fromstring(str, np.uint8)
 	return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+def cv_bs64(img_np):
+	image = cv2.imencode('.png', img_np)[1]
+	base64_data = str(base64.b64encode(image))[2:-1]
+	return 'data:image/png;base64,'+base64_data
 
 @app.route('/')
 @app.route('/generate')
@@ -37,11 +40,9 @@ def upload_mark():
 	data=data['data']
 	img=data['base64']
 	img_np = bs64_cv(img)
-	img_np=img_np//64*64
+	img_np=img_np//128*128
 	cv2.imwrite("mark.png", img_np, [int(cv2.IMWRITE_PNG_COMPRESSION), 0]) 
-	image = cv2.imencode('.png', img_np)[1]
-	base64_data = str(base64.b64encode(image))[2:-1]
-	base64_data='data:image/png;base64,'+base64_data
+	base64_data=cv_bs64(img_np)
 	return jsonify(data=base64_data)
 @app.route('/upload_img',methods=['POST'])
 def upload_img():
@@ -51,9 +52,7 @@ def upload_img():
 	img_np =bs64_cv(img)
 	img_np=img_np//4*4
 	cv2.imwrite("img.png", img_np, [int(cv2.IMWRITE_PNG_COMPRESSION), 0]) 
-	image = cv2.imencode('.png', img_np)[1]
-	base64_data = str(base64.b64encode(image))[2:-1]
-	base64_data='data:image/png;base64,'+base64_data
+	base64_data=cv_bs64(img_np)
 	return jsonify(data=base64_data)
 @app.route('/generate_img',methods=['POST'])
 def generate_img():
@@ -61,9 +60,7 @@ def generate_img():
 	mark=cv2.imread("mark.png")
 	mark = cv2.resize(mark, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_CUBIC)
 	new_img=mark//64+img
-	image = cv2.imencode('.png', new_img)[1]
-	base64_data = str(base64.b64encode(image))[2:-1]
-	base64_data='data:image/png;base64,'+base64_data
+	base64_data=cv_bs64(new_img)
 	return jsonify(data=base64_data)
 
 @app.route('/extract_img',methods=['POST'])
@@ -74,7 +71,5 @@ def extract_img():
 	img_np = bs64_cv(img)
 	img_np=img_np%4*64
 	cv2.imwrite("img.png", img_np, [int(cv2.IMWRITE_PNG_COMPRESSION), 0]) 
-	image = cv2.imencode('.png', img_np)[1]
-	base64_data = str(base64.b64encode(image))[2:-1]
-	base64_data='data:image/png;base64,'+base64_data
+	base64_data=cv_bs64(img_np)
 	return jsonify(data=base64_data)
